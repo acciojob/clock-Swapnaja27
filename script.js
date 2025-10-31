31554084e101e267bc8b0362950b001e31100394
Swapnaja27 <142212367+Swapnaja27@users.noreply.github.com>
2025-10-31 09:55:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let clock=document.getElementById("timer");
+function showtime(){
+	let date=new Date();
+	let time=date.toLocaleString();
+	clock.textContent=time;
+}
+showtime();
+
+setInterval(showtime,1000);
